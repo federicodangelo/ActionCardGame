431d1dbb5886ff6d99e02bd7f084dfca094d4694
Federico D'Angelo <federicodangelo@gmail.com>
2018-02-19 22:57:21 -0300

    Updated CardView

--- src/components/CardView.js
+++ src/components/CardView.js
@@ -1,11 +1,12 @@
 //@flow
 
 import React, {Component} from "react";
-import {Image, Text, View} from "react-native";
+import {Image, Text, View, TouchableHighlight, Alert} from "react-native";
 import {CardDefinition} from "../model/Cards"
 
 interface CardViewProps {
-  card:CardDefinition
+  id:number;
+  card:CardDefinition;
 }
 
 export default class CardView extends Component<CardViewProps> {
@@ -13,17 +14,23 @@ export default class CardView extends Component<CardViewProps> {
     render() {
 
         return (
-            <View style={{padding: 2}}>
-                <View style={{backgroundColor: "black", paddingBottom: 2}}>
-                    <Image
-                        style={{width: 100}}
-                        resizeMode="contain"
-                        source={this.props.card.icon}
-                    />
-                    <Text style={{color: "white", textAlign: "center"}}>{this.props.card.name}</Text>
-                </View>
+            <View style={{padding: "0.5%", width: "25%"}}>
+                <TouchableHighlight onPress={() => this.onCardClicked(this.props.id, this.props.card) }>
+                    <View style={{backgroundColor: "black", paddingBottom: "5%"}}>
+                        <Image
+                            style={{width: "100%"}}
+                            resizeMode="contain"
+                            source={this.props.card.icon}
+                        />
+                        <Text numberOfLines={1} style={{color: "white", textAlign: "center"}}>{this.props.card.name}</Text>
+                    </View>
+                </TouchableHighlight>
             </View>
         );
+    }
+
+    onCardClicked(id:number, card:CardDefinition) : void {
+        Alert.alert(`Clicked on card ${card.name} with id ${id}`)
     }
 
 }
